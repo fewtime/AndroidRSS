3934f0085bf9607a1f1fc9e3bea24669318dbf7e
cowlog <cowlog.li@gmail.com>
2017-11-13 13:32:51 +0800

    添加欢迎页面

--- app/src/main/java/com/example/androidrss/MainActivity.java
+++ app/src/main/java/com/example/androidrss/MainActivity.java
@@ -1,103 +1,52 @@
 package com.example.androidrss;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
 
-import com.example.androidrss.Adapter.FeedAdapter;
-import com.example.androidrss.Common.HTTPDataHandler;
-import com.example.androidrss.Model.RSSObject;
-import com.google.gson.Gson;
-import com.scwang.smartrefresh.layout.api.RefreshLayout;
-import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
-import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+import com.example.androidrss.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    Toolbar toolbar;
-    RecyclerView recyclerView;
-    RSSObject rssObject;
-
-    // RSS link
-    private final String RSS_link = "http://news.qq.com/newsgn/rss_newsgn.xml";
-    private final String RSS_to_Json_API = "https://api.rss2json.com/v1/api.json?rss_url=";
-
+public class MainActivity extends Activity {
+    private ImageView welcomeImg = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        welcomeImg = (ImageView) this.findViewById(R.id.welcomeImg);
+        AlphaAnimation anima = new AlphaAnimation(0.3f, 1.0f);
+        anima.setDuration(3000);
+        welcomeImg.startAnimation(anima);
+        anima.setAnimationListener(new AnimationImpl());
 
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        toolbar.setTitle("News");
-        setSupportActionBar(toolbar);
-
-        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getBaseContext(),
-                LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        RefreshLayout refreshLayout = (RefreshLayout)findViewById(R.id.refreshLayout);
-        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
-            @Override
-            public void onRefresh(RefreshLayout refreshlayout) {
-                loadRSS();
-                refreshlayout.finishRefresh(2000);
-            }
-        });
-        refreshLayout.setOnLoadmoreListener(new OnLoadmoreListener() {
-            @Override
-            public void onLoadmore(RefreshLayout refreshlayout) {
-                refreshlayout.finishLoadmore(2000);
-            }
-        });
-
-
-        loadRSS();
     }
 
-    private void loadRSS() {
-        AsyncTask<String, String, String> loadRSSAsync = new AsyncTask<String, String, String>() {
+    private class AnimationImpl implements AnimationListener {
 
-            @Override
-            protected String doInBackground(String... params) {
-                String result;
-                HTTPDataHandler http = new HTTPDataHandler();
-                result = http.GetHTTPData(params[0]);
-                return result;
-            }
+        @Override
+        public void onAnimationStart(Animation animation) {
+            welcomeImg.setBackgroundResource(R.drawable.welcome);
+        }
 
-            @Override
-            protected void onPostExecute(String s) {
-//                mDialog.dismiss();
-                rssObject = new Gson().fromJson(s, RSSObject.class);
-                FeedAdapter adapter = new FeedAdapter(rssObject, getBaseContext());
-                recyclerView.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
-            }
-        };
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            skip();
+        }
 
-        StringBuilder url_get_data = new StringBuilder(RSS_to_Json_API);
-        url_get_data.append(RSS_link);
-        loadRSSAsync.execute(url_get_data.toString());
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.main_menu, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (item.getItemId() == R.id.menu_refresh)
-//            loadRSS();
-//        return true;
-//    }
+    private void skip() {
+        startActivity(new Intent(this, NewsList.class));
+        finish();
+    }
 }
